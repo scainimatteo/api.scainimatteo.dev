49ca2b9e0b9c01a16bf4e32f5f028d5ff7d449a2
scainimatteo <info@scainimatteo.dev>
2026-08-18 08:35:39 +0200

    Moved placeholder to specific file

---
+++ outline/constants.go
@@ -0,0 +1,33 @@
+package outline
+
+var monthTablePlaceholderTitle = "Copia template del mese negli appunti"
+var monthTablePlaceholderPlain = `Descrizione
+Categoria
+Importo
+Eseguito
+Bolletta internet MM/YY
+Bollette
+50
+`
+var monthTablePlaceholderHTML = `<table>
+  <colgroup>
+    <col style="width: 421px;">
+    <col style="width: 123px;">
+    <col style="width: 96px;">
+    <col style="width: 96px;">
+  </colgroup>
+  <tbody>
+    <tr>
+      <th data-colwidth="421"><p dir="auto">Descrizione</p></th>
+      <th data-colwidth="123"><p dir="auto">Categoria</p></th>
+      <th data-colwidth="96"><p dir="auto">Importo</p></th>
+      <th data-colwidth="96"><p dir="auto">Eseguito</p></th>
+    </tr>
+    <tr>
+      <td data-colwidth="421"><p dir="auto">Bolletta internet 08/26</p></td>
+      <td data-colwidth="123"><p dir="auto">Bollette</p></td>
+      <td data-colwidth="96"><p dir="auto">50</p></td>
+      <td data-colwidth="96"><p dir="auto"></p></td>
+    </tr>
+  </tbody>
+</table>`
--- outline/handler.go
+++ outline/handler.go
@@ -21,37 +21,6 @@ var sumListTemplate string
 
 //go:embed templates/copy-text.html
 var copyTextTemplate string
-var monthTablePlaceholderTitle = "Copia template del mese negli appunti"
-var monthTablePlaceholderPlain = `Descrizione
-Categoria
-Importo
-Eseguito
-Bolletta internet MM/YY
-Bollette
-50
-`
-var monthTablePlaceholderHTML = `<table>
-  <colgroup>
-    <col style="width: 421px;">
-    <col style="width: 123px;">
-    <col style="width: 96px;">
-    <col style="width: 96px;">
-  </colgroup>
-  <tbody>
-    <tr>
-      <th data-colwidth="421"><p dir="auto">Descrizione</p></th>
-      <th data-colwidth="123"><p dir="auto">Categoria</p></th>
-      <th data-colwidth="96"><p dir="auto">Importo</p></th>
-      <th data-colwidth="96"><p dir="auto">Eseguito</p></th>
-    </tr>
-    <tr>
-      <td data-colwidth="421"><p dir="auto">Bolletta internet 08/26</p></td>
-      <td data-colwidth="123"><p dir="auto">Bollette</p></td>
-      <td data-colwidth="96"><p dir="auto">50</p></td>
-      <td data-colwidth="96"><p dir="auto"></p></td>
-    </tr>
-  </tbody>
-</table>`
 
 func (s OutlineService) GetTemplate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
